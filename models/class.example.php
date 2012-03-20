c073e2b264ee54ce65d466007005cfb2f4777b3b
kristovaher <kristo@waher.net>
2012-03-21 01:20:10 +0200

    Added files

---
+++ models/class.example.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+WWW - PHP micro-framework
+MVC Model class
+
+Minimal example model for demonstration purposes.
+
+Author and support: Kristo Vaher - kristo@waher.net
+*/
+
+// WWW_Factory is parent class for all MVC classes of WWW
+class WWW_model_example extends WWW_Factory {
+
+	// All the variables of this model should be stored here
+	public $id=0;
+	public $name='';
+	
+	// This is like __construct() but for Factory-created objects
+	public function __initialize(){
+	
+		// Do something here
+		return true;
+		
+	}
+	
+	// This is intended to load data from database
+	// * id - Identifier of the object loaded
+	public function load($id){
+	
+		// Actual database query should be built here
+		$this->id=$id;
+		$this->name='Lorem Ipsum';
+		return true;
+		
+	}
+	
+	// This returns all of the data of currently open object
+	public function get(){
+	
+		// Data is returned as an array
+		return array(
+			'id' => $this->id,
+			'name'=>$this->name
+		);
+		
+	}
+
+	// This function is intended to save data to database
+	public function save(){
+	
+		// Actual database query should be built here
+		return true;
+		
+	}
+
+}
+	
+?>

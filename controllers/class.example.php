515b2c294ee968d8dd08c8c0cb789391f8785235
kristovaher <kristo@waher.net>
2012-04-26 23:19:30 +0300

    API Information tool now also fetches comments of a method if they are formatted between /** and */

--- controllers/class.example.php
+++ controllers/class.example.php
@@ -11,8 +11,8 @@ Author and support: Kristo Vaher - kristo@waher.net
 
 // WWW_Factory is parent class for all MVC classes of WWW
 class WWW_controller_example extends WWW_Factory {
-
-	// Simple example call (from API it would be 'example-get' commmand
+	
+	/** Simple example call (from API it would be 'example-get' command */
 	public function get(){
 		// New objects can be created through Factory easily
 		$example=$this->getModel('example');
